ba770a715bc637826e3c85bd837e2f5dba01717a
Cristian Ocaña Soler <cristian.ocana@onvia.es>
2022-07-04 16:06:46 +0200

    Wordpress kit with Basic Block working (Need yet JS and CSS)

---
+++ site/web/app/themes/wordpresskit/functions.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our theme. We will simply require it into the script here so that we
+| don't have to worry about manually loading any of our classes later on.
+|
+*/
+
+if (! file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
+    wp_die(__('Error locating autoloader. Please run <code>composer install</code>.', 'sage'));
+}
+
+require $composer;
+
+/*
+|--------------------------------------------------------------------------
+| Register The Bootloader
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is schedule a new Acorn application container
+| to boot when WordPress is finished loading the theme. The application
+| serves as the "glue" for all the components of Laravel and is
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+try {
+    \Roots\bootloader();
+} catch (Throwable $e) {
+    wp_die(
+        __('You need to install Acorn to use this theme.', 'sage'),
+        '',
+        [
+            'link_url' => 'https://docs.roots.io/acorn/2.x/installation/',
+            'link_text' => __('Acorn Docs: Installation', 'sage'),
+        ]
+    );
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register Sage Theme Files
+|--------------------------------------------------------------------------
+|
+| Out of the box, Sage ships with categorically named theme files
+| containing common functionality and setup to be bootstrapped with your
+| theme. Simply add (or remove) files from the array below to change what
+| is registered alongside Sage.
+|
+*/
+
+collect(['setup', 'filters'])
+    ->each(function ($file) {
+        if (! locate_template($file = "app/{$file}.php", true, true)) {
+            wp_die(
+                /* translators: %s is replaced with the relative file path */
+                sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file)
+            );
+        }
+    });
+
+/*
+|--------------------------------------------------------------------------
+| Enable Sage Theme Support
+|--------------------------------------------------------------------------
+|
+| Once our theme files are registered and available for use, we are almost
+| ready to boot our application. But first, we need to signal to Acorn
+| that we will need to initialize the necessary service providers built in
+| for Sage when booting.
+|
+*/
+
+add_theme_support('sage');
+
+// Hide acf menu item in production environment
+if (WP_ENV === 'production') {
+    add_filter( 'acf/settings/show_admin', '__return_false' );
+}
+
+// Block registers
+add_action('acf/init', 'AcfRegisterBlocks');
+function AcfRegisterBlocks() {
+    // Check function exists.
+    if( function_exists('acf_register_block_type') ) {
+        // register a testimonial block.
+        acf_register_block_type(array(
+            'name'              => 'testimonial',
+            'title'             => __('Testimonial'),
+            'description'       => __('A custom testimonial block.'),
+            'render_template'   => "resources/views/blocks/testimonial.blade.php",
+            'render_callback' => function( $block ) {
+                $slug             = str_replace( 'acf/', '', $block['name'] );
+                $block['slug']    = $slug;
+                $block['classes'] = implode( ' ', [ $block['slug'], $block['align'] ] );
+                echo view("blocks/${slug}", [ 'block' => $block ]);
+            },
+            'category'          => 'formatting',
+            'icon'              => 'admin-comments',
+            'keywords'          => array( 'testimonial', 'quote' ),
+        ));
+    }
+}
---
+++ site/web/app/themes/wordpresskit/resources/views/blocks/testimonial.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Testimonial Block Template.
+*
+* @param   array $block The block settings and attributes.
+* @param   string $content The block inner HTML (empty).
+* @param   bool $is_preview True during AJAX preview.
+* @param   (int|string) $post_id The post ID this block is saved to.
+*/
+?>
+<section>
+  <h1>{{ get_field("name") }}</h1>
+  <h2>{{ get_field("description") }}</h2>
+</section>
